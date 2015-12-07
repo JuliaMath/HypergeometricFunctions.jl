4f01a023802f6ca4cd4b9569c2c254ca56b3a66a
Sheehan Olver <solver@mac.com>
2015-12-08 09:55:58 +1100

    Fix reverseorientation tests

--- runtests.jl
+++ runtests.jl
@@ -124,23 +124,24 @@ f=Fun(z->exp(exp(0.1im)*z+1/z),Laurent(Circle()))
 
 @test_approx_eq hilbert(f,exp(0.2im)) hilbert(Fun(f,Fourier),exp(0.2im))
 @test_approx_eq hilbert(f,exp(0.2im)) -hilbert(reverseorientation(f),exp(0.2im))
-@test_approx_eq hilbert(f,exp(0.2im)) hilbert(reverseorientation(Fun(f,Fourier)),exp(0.2im))
+@test_approx_eq hilbert(f,exp(0.2im)) -hilbert(reverseorientation(Fun(f,Fourier)),exp(0.2im))
 
 @test_approx_eq cauchy(f,0.5exp(0.2im)) cauchy(Fun(f,Fourier),0.5exp(0.2im))
 @test_approx_eq cauchy(f,0.5exp(0.2im)) -cauchy(reverseorientation(f),0.5exp(0.2im))
-@test_approx_eq cauchy(f,0.5exp(0.2im)) cauchy(reverseorientation(Fun(f,Fourier)),0.5exp(0.2im))
-@test_approx_eq cauchy(f,0.5exp(0.2im)) (OffHilbert(domainspace(f),Laurent(Circle(0.5)))*f)(0.5exp(0.2im))/(2im)
+@test_approx_eq cauchy(f,0.5exp(0.2im)) -cauchy(reverseorientation(Fun(f,Fourier)),0.5exp(0.2im))
+@test_approx_eq cauchy(f,0.5exp(0.2im)) (OffHilbert(space(f),Laurent(Circle(0.5)))*f)(0.5exp(0.2im))/(2im)
 
-f=Fun(z->exp(exp(0.1im)*z+1/z),Laurent(Circle(1.,0.5)))
+f=Fun(z->exp(exp(0.1im)*z+1/(z-1.)),Laurent(Circle(1.,0.5)))
 @test_approx_eq cauchy(f,0.5exp(0.2im)) cauchy(Fun(f,Fourier),0.5exp(0.2im))
 @test_approx_eq cauchy(f,0.5exp(0.2im)) -cauchy(reverseorientation(f),0.5exp(0.2im))
-@test_approx_eq cauchy(f,0.5exp(0.2im)) cauchy(reverseorientation(Fun(f,Fourier)),0.5exp(0.2im))
+@test_approx_eq cauchy(f,0.5exp(0.2im)) -cauchy(reverseorientation(Fun(f,Fourier)),0.5exp(0.2im))
 
 
 Γ=Circle()∪Circle(0.5)
 f=depiece([Fun(z->z^(-1),Γ[1]),Fun(z->z,Γ[2])])
 A=I-(f-Fun(one,space(f)))*Cauchy(-1)
 u=A\(f-Fun(one,space(f)))
+
 @test_approx_eq 1+cauchy(u,.1) 1
 @test_approx_eq 1+cauchy(u,.8) 1/0.8
 @test_approx_eq 1+cauchy(u,2.) 1
