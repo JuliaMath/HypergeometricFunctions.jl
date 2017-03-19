927c1a35814d661200aa4e3f6160d6f7c89de419
Richard Slevinsky <Richard.Slevinsky@umanitoba.ca>
2017-03-19 18:56:48 -0500

    mainly deprecation warnings
    tests don’t quite pass yet, but
    
    test_approx_eq is removed,
    
    Array(T,n) -> Array{T}(n)
    
    chebyshevtransform -> p*
    
    log(abs()) -> logabs()
    
    logabs() = log(abs2())/2 is better because abs2 doesn’t need roots

--- runtests.jl
+++ runtests.jl
@@ -12,43 +12,42 @@ println("Special function tests")
 
 
 for (a,b,c) in ((1,1,2),(2,2,4)), x in (1.1,10.1,1.5)
-    @test_approx_eq _₂F₁(a,b,c,x+eps()im) _₂F₁(a,b,c,x*⁺)
-    @test_approx_eq _₂F₁(a,b,c,x-eps()im) _₂F₁(a,b,c,x*⁻)
+    @test _₂F₁(a,b,c,x+eps()im) ≈ _₂F₁(a,b,c,x*⁺)
+    @test _₂F₁(a,b,c,x-eps()im) ≈ _₂F₁(a,b,c,x*⁻)
 end
 
 
-@test_approx_eq x̄sqrtx2real(2.0+3.0im) real(sqrtx2(2.0+3.0im)*(2.0-3.0im))
+@test x̄sqrtx2real(2.0+3.0im) ≈ real(sqrtx2(2.0+3.0im)*(2.0-3.0im))
 
 for s in (true,false)
     for z in (2.0+3.0im,2.0,0.1)
-        @test_approx_eq real(joukowskyinverse(Val{s},z+0im)) joukowskyinversereal(Val{s},z)
-        @test_approx_eq abs(joukowskyinverse(Val{s},z+0im)) joukowskyinverseabs(Val{s},z)
+        @test real(joukowskyinverse(Val{s},z+0im)) ≈ joukowskyinversereal(Val{s},z)
+        @test abs(joukowskyinverse(Val{s},z+0im)) ≈ joukowskyinverseabs(Val{s},z)
     end
 
     p=0.1
-    @test_approx_eq joukowskyinverse(Val{s},p*⁺) joukowskyinverse(Val{s},p+0im)
-    @test_approx_eq joukowskyinverse(Val{s},p*⁻) joukowskyinverse(Val{s},p-0im)
+    @test joukowskyinverse(Val{s},p*⁺) ≈ joukowskyinverse(Val{s},p+0im)
+    @test joukowskyinverse(Val{s},p*⁻) ≈ joukowskyinverse(Val{s},p-0im)
 end
 
 
 x=Fun()
-@test_approx_eq sum(log(abs(x-2.0))) logabslegendremoment(2.0)
-@test_approx_eq sum(log(abs(x-(2.0+im)))) logabslegendremoment(2.0+im)
+@test sum(logabs(x-2.0)) ≈ logabslegendremoment(2.0)
+@test sum(logabs(x-(2.0+im))) ≈ logabslegendremoment(2.0+im)
 
-@test_approx_eq stieltjesmoment(Legendre(),0,0.1+0im) stieltjesmoment(Legendre(),0,Directed{true}(0.1))
-@test_approx_eq stieltjesmoment(Legendre(),0,0.1-0im) stieltjesmoment(Legendre(),0,Directed{false}(0.1))
-@test_approx_eq stieltjesjacobimoment(0.,0.,1,0.1+0im) stieltjesjacobimoment(0.,0.,1,0.1*⁺)
-@test_approx_eq stieltjesjacobimoment(0.,0.,1,0.1-0im) stieltjesjacobimoment(0.,0.,1,0.1*⁻)
+@test stieltjesmoment(Legendre(),0,0.1+0im) ≈ stieltjesmoment(Legendre(),0,Directed{true}(0.1))
+@test stieltjesmoment(Legendre(),0,0.1-0im) ≈ stieltjesmoment(Legendre(),0,Directed{false}(0.1))
+@test stieltjesjacobimoment(0.,0.,1,0.1+0im) ≈ stieltjesjacobimoment(0.,0.,1,0.1*⁺)
+@test stieltjesjacobimoment(0.,0.,1,0.1-0im) ≈ stieltjesjacobimoment(0.,0.,1,0.1*⁻)
 
-@test_approx_eq (stieltjesjacobimoment(0,0,0,0.1+0im)-stieltjesjacobimoment(0,0,0,0.1-0im))/(-2π*im) 1.0
-@test_approx_eq (stieltjesjacobimoment(0,0,1,0.1+0im)-stieltjesjacobimoment(0,0,1,0.1-0im))/(-2π*im) 0.1
+@test (stieltjesjacobimoment(0,0,0,0.1+0im)-stieltjesjacobimoment(0,0,0,0.1-0im))/(-2π*im) ≈ 1.0
+@test (stieltjesjacobimoment(0,0,1,0.1+0im)-stieltjesjacobimoment(0,0,1,0.1-0im))/(-2π*im) ≈ 0.1
 
 println("Hilbert test")
 include("HilbertTest.jl")
 
 println("Stieltjes moment test")
 include("stieltjesmomentTest.jl")
-
 
 println("Stieltjes integral test")
 include("stieltjesintegraltest.jl")
