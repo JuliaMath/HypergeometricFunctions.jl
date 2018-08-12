8a25fd0869299386c733755f1907375ddd805aeb
Sheehan Olver <solver@mac.com>
2018-08-12 16:48:04 +0100

    bump REQUIRE, remove Compat

--- runtests.jl
+++ runtests.jl
@@ -1,4 +1,4 @@
-using ApproxFun, SingularIntegralEquations, Compat.Test
+using ApproxFun, SingularIntegralEquations, Test
     import SingularIntegralEquations: x̄sqrtx2real, sqrtx2, joukowskyinverse,
             joukowskyinversereal, joukowskyinverseabs, ⁺, ⁻, logabslegendremoment,
             stieltjeslegendremoment, stieltjesjacobimoment, stieltjesmoment, Directed,
