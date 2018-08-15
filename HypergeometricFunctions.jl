fcc0de1c7b0893060a52ae179723f9c23d22de19
Sheehan Olver <solver@mac.com>
2018-08-15 16:33:30 +0100

    using works in Julia 0.7

--- HypergeometricFunctions.jl
+++ HypergeometricFunctions.jl
@@ -4,7 +4,7 @@ This module calculates (generalized) hypergeometric functions:
     mFn(a;b;z) = Σ_{k=0}^∞ (a_1,k) ⋯ (a_m,k) / (b_1,k) ⋯ (b_n,k) zᵏ/k!
 """
 module HypergeometricFunctions
-    using DualNumbers
+    using DualNumbers, SpecialFunctions
 import ApproxFun: @clenshaw, real, eps, reverseorientation
 import FastTransforms: pochhammer
 import SingularIntegralEquations: undirected, Directed
