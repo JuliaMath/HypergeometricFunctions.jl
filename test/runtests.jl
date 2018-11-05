e76e095d92aa4f8ce0a8b61ec159b971f8b87d22
James Cook <cookjws@gmail.com>
2018-11-05 20:30:15 +0000

    #4 dont need two test files

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,31 @@
-using ApproxFun,  SpecialFunctions, LinearAlgebra, Test
+using HyperGeometricFunctions, Test
+#using ApproxFun,  SpecialFunctions, LinearAlgebra, Test
+
+@testset "Hypergeometric Function tests" begin
+  #include("hypergeometricTest.jl")
+
+    const e = exp(1.0)
+    for z in (.9rand(Float64,10),10rand(ComplexF64,10))
+        j=1
+        for (a,b,c) in ((√2/e,1.3,1.3),(1.2,√3,1.2),
+                        (-.4,.4,.5),(-.3,1.3,.5),(.35,.85,.5),
+                        (.5,.5,1.5),(1.,1.,1.5),(.5,1.,1.5),(.3,.7,1.5),(.7,1.3,1.5),(.35,.85,1.5),
+                        (1.,1.,2.),(3.,1.,2.),(-2.,1.,2.),(-3.,1.,2.),(1.,-4.,2.),
+                        (2.,2.,4.),(1.,1.5,2.5))
+            normj = 0.0
+            for zi in z
+                twoFone = _₂F₁(a,b,c,zi)
+                aa,bb,cc = big(a),big(b),big(c)
+                twoFonegeneral = convert(Complex{Float64},_₂F₁general(aa,bb,cc,big(zi)))
+                norm(twoFone/twoFonegeneral-1) > sqrt(eps()) && println("This is ₂F₁($a,$b;$c;zi) - ₂F₁general($a,$b;$c;zi): ",norm(twoFone/twoFonegeneral-1),"   ",twoFone,"   ",twoFonegeneral,"   ",isfinite(twoFone),"   ",isfinite(twoFonegeneral)," this is zi: ",zi)
+                normj += Float64(norm(twoFone/twoFonegeneral-1))
+            end
+            println("This is the cumulative error for Case $j: ",normj)
+            @test normj < 6.0e-14
+            j+=1
+        end
+    end
 
 
-@testset "Hypergeometric Functiontests" begin
-  include("hypergeometricTest.jl")
 end
 
