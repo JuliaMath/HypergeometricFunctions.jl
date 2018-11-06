ee5a9170aeff85eed996de6329c2d31535b50904
James Cook <cookjws@gmail.com>
2018-11-06 07:56:52 +0000

    #3 tests revamped. does using rather than include

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,10 @@
-using HyperGeometricFunctions, Test
-#using ApproxFun,  SpecialFunctions, LinearAlgebra, Test
+using HypergeometricFunctions, Test
+import LinearAlgebra: norm
+import HypergeometricFunctions: _₂F₁general # not exported 
 
 @testset "Hypergeometric Function tests" begin
-  #include("hypergeometricTest.jl")
-
-    const e = exp(1.0)
+    e = exp(1.0)
+    regression_max_accumulated_error = 6.0e-14 # discovered by running the test
     for z in (.9rand(Float64,10),10rand(ComplexF64,10))
         j=1
         for (a,b,c) in ((√2/e,1.3,1.3),(1.2,√3,1.2),
@@ -12,20 +12,18 @@ using HyperGeometricFunctions, Test
                         (.5,.5,1.5),(1.,1.,1.5),(.5,1.,1.5),(.3,.7,1.5),(.7,1.3,1.5),(.35,.85,1.5),
                         (1.,1.,2.),(3.,1.,2.),(-2.,1.,2.),(-3.,1.,2.),(1.,-4.,2.),
                         (2.,2.,4.),(1.,1.5,2.5))
-            normj = 0.0
+            error_accum = 0.0
             for zi in z
                 twoFone = _₂F₁(a,b,c,zi)
                 aa,bb,cc = big(a),big(b),big(c)
                 twoFonegeneral = convert(Complex{Float64},_₂F₁general(aa,bb,cc,big(zi)))
                 norm(twoFone/twoFonegeneral-1) > sqrt(eps()) && println("This is ₂F₁($a,$b;$c;zi) - ₂F₁general($a,$b;$c;zi): ",norm(twoFone/twoFonegeneral-1),"   ",twoFone,"   ",twoFonegeneral,"   ",isfinite(twoFone),"   ",isfinite(twoFonegeneral)," this is zi: ",zi)
-                normj += Float64(norm(twoFone/twoFonegeneral-1))
+                error_accum += Float64(norm(twoFone/twoFonegeneral-1))
             end
-            println("This is the cumulative error for Case $j: ",normj)
-            @test normj < 6.0e-14
+            println("This is the cumulative error for Case $j: ", error_accum)
+            @test error_accum < regression_max_accumulated_error
             j+=1
         end
     end
-
-
 end
 
